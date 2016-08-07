f469f19a1d499cd852e057a2d4e0b2279e974ff1
maldicion069 <ccrisrober@gmailcom>
2016-08-07 16:55:50 +0200

    Added default configuration. Implemented upload_fields_files

--- README.md
+++ README.md
@@ -7,21 +7,37 @@ Simple file uploader for NodeJS applications
 	// First parameter: Optional callback for rename (only replace if defined)
 	// Second parameter: Optional uploading route (Default: "./uploads/") 
 	//		NOTE: Create route if not exist
+	// Third parameter: Optional configuration
 	var upload = new NodeUpload(function() {
 		return "FOO" + Math.random();
-	}, "./myroute/");
-	// Simple file
-		// No extensions (default: [.png, .jpg, .jpeg])
-		upload.upload_file(req, res, "photo", null, {});
-		// Simple extension
-		upload.upload_file(req, res, "photo", "png", {});								(Dot is deleted when you check the type. Case sensitive extensions)
-		// Array extension
-		upload.upload_file(req, res, "photo", ["png"], {});								(Dot is deleted when you check the type. Case sensitive extensions)
+	}, "./myroute/", {
+		fieldSize: 15
+	});
+
+	// Simple file (Dot is deleted when you check the type. Case sensitive extensions)
+	// No extensions (default: [.png, .jpg, .jpeg])
+	upload.upload_file(req, res, "photo", null, {});
+	// Simple extension (Dot is deleted when you check the type. Case sensitive extensions)
+	upload.upload_file(req, res, "photo", "png", {});
+	// Array extension (Dot is deleted when you check the type. Case sensitive extensions)
+	upload.upload_file(req, res, "photo", ["png"], {});
+
 	// Array of files
-		// No extensions (default: [.png, .jpg, .jpeg])
-		upload.upload_array_files(req, res, "photo", null, { files: 2 });
-		// Simple extension
-		upload.upload_array_files(req, res, "photo", ".png", { files: 2 });				(Dot is deleted when you check the type. Case sensitive extensions)
-		// Array extension
-		upload.upload_array_files(req, res, "photo", [".png", "jpg"], { files: 2 });	(Dot is deleted when you check the type. Case sensitive extensions)
+	// No extensions (default: [.png, .jpg, .jpeg])
+	upload.upload_array_files(req, res, "photo", null, { files: 2 });
+	// Simple extensions (Dot is deleted when you check the type. Case sensitive extensions)
+	upload.upload_array_files(req, res, "photo", ".png", { files: 2 });
+	// Array extension (Dot is deleted when you check the type. Case sensitive extensions)
+	upload.upload_array_files(req, res, "photo", [".png", "jpg"], { files: 2 });
+
+	// Array of fields
+	// No extensions (default: [.png, .jpg, .jpeg])
+	upload.upload_array_files(req, res, 
+		[ { name: 'avatar', maxCount: 1 }, { name: 'gallery', maxCount: 8 } ], null, { files: 2 });
+	// Simple extension (Dot is deleted when you check the type. Case sensitive extensions)
+	upload.upload_array_files(req, res, 
+		[ { name: 'avatar', maxCount: 1 }, { name: 'gallery', maxCount: 8 } ], ".png", { files: 2 });
+	// Array extension (Dot is deleted when you check the type. Case sensitive extensions)
+	upload.upload_array_files(req, res, 
+		[ { name: 'avatar', maxCount: 1 }, { name: 'gallery', maxCount: 8 } ] , [".png", "jpg"], { files: 2 });
 ```
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function extend(target) {
-    var sources = [].slice.call(arguments, 1);
-    sources.forEach(function (source) {
-        for (var prop in source) {
-            target[prop] = source[prop];
-        }
-    });
-    return target;
+	var sources = [].slice.call(arguments, 1);
+	sources.forEach(function (source) {
+		for (var prop in source) {
+			target[prop] = source[prop];
+		}
+	});
+	return target;
 }
 var path = require("path");
 var multer = require("multer");
@@ -15,71 +15,75 @@ var _isFunction = function(obj) {
 };
 
 var NodeUpload = (function () {
-    function NodeUpload(filenameFN, routeStr) {
-        if (_isFunction(filenameFN)) {
-            NodeUpload.prototype._filename = filenameFN;
-        }
-        var fs = require('fs');
-        this.route = routeStr || NodeUpload._defDirectory;
+	function NodeUpload(filenameFN, routeStr, defConfig) {
+		if (_isFunction(filenameFN)) {
+			NodeUpload.prototype._filename = filenameFN;
+		}
+		var fs = require('fs');
+		this.route = routeStr || NodeUpload._defDirectory;
 
 		if (!fs.existsSync(this.route)){
-	    	fs.mkdirSync(this.route);
+			fs.mkdirSync(this.route);
 		}
-        var self = this;
-        this.storage = multer.diskStorage({
-            destination: function (req, file, cb) {
-                cb(null, self.route);	//NodeUpload._defDirectory);
-            },
-            filename: function (req, file, cb) {
-                var name = file.originalname.substr(0, file.originalname.lastIndexOf('.'));
-		        var ext = path.extname(file.originalname).toLowerCase();
+
+		// Default configuration
+		this.defConfig = extend({}, NodeUpload._limits, defConfig || {});
+
+		var self = this;
+		this.storage = multer.diskStorage({
+			destination: function (req, file, cb) {
+				cb(null, self.route);	//NodeUpload._defDirectory);
+			},
+			filename: function (req, file, cb) {
+				var name = file.originalname.substr(0, file.originalname.lastIndexOf('.'));
+				var ext = path.extname(file.originalname).toLowerCase();
 				if (ext.substring(0, 1) == '.') { 
 					ext = ext.substring(1);
 				}
-                cb(null, self._filename(name) + ext);
-            }
-        });
-        this.upload = multer({
-            storage: this.storage,
-            dest: this.route,	//NodeUpload._defDirectory,
-            fileFilter: NodeUpload.fileFilter,
-            limits: NodeUpload._limits
-        });
-    }
-    NodeUpload.removeDotsExtension = function(exts) {
-    	var ext;
-    	for(var i = 0; i < exts.length; i++) {
-    		ext = exts[i];
-    		if (ext.substring(0, 1) == '.') { 
+				cb(null, self._filename(name) + ext);
+			}
+		});
+		this.upload = multer({
+			storage: this.storage,
+			dest: this.route,	//NodeUpload._defDirectory,
+			fileFilter: NodeUpload.fileFilter,
+			limits: this.defConfig
+		});
+	}
+	NodeUpload.removeDotsExtension = function(exts) {
+		var ext;
+		for(var i = 0; i < exts.length; i++) {
+			ext = exts[i];
+			if (ext.substring(0, 1) == '.') { 
 				ext = ext.substring(1);
 			}
 			exts[i] = ext;
-    	}
-    	return exts;
-    }
-    NodeUpload.prototype.upload_file = function (req, res, fieldName, types, config) {
-        req.extensions = (types && typeof types !== "undefined") ? (types instanceof Array) ? 
-        	NodeUpload.removeDotsExtension(types) : NodeUpload.removeDotsExtension([types]) : NodeFilter._defTypes;
+		}
+		return exts;
+	}
+	NodeUpload.prototype.upload_file = function (req, res, fieldName, types, config) {
+		req.extensions = (types && typeof types !== "undefined") ? (types instanceof Array) ? 
+			NodeUpload.removeDotsExtension(types) : NodeUpload.removeDotsExtension([types]) : NodeFilter._defTypes;
 
-        this.upload.limits = extend({}, NodeUpload._limits, config || {});
+		this.upload.limits = extend({}, this.defConfig, config || {});
 
-        this.upload.single(fieldName)(req, res, function (err) {
-            if (err || req.fileValidationError) {
-                return res.json({ "err": err || req.fileValidationError });
-            }
-            else if (!req.file) {
-                return res.json({ "err": "File dont founded" });
-            }
-            res.json(req.file);
-        });
-    };
-    NodeUpload.prototype.upload_array_files = function (req, res, fieldName, types, config) {
-        req.extensions = (types && typeof types !== "undefined") ? (types instanceof Array) ? 
-        	NodeUpload.removeDotsExtension(types) : NodeUpload.removeDotsExtension([types]) : NodeFilter._defTypes;
+		this.upload.single(fieldName)(req, res, function (err) {
+			if (err || req.fileValidationError) {
+				return res.json({ "err": err || req.fileValidationError });
+			}
+			else if (!req.file) {
+				return res.json({ "err": "File dont founded" });
+			}
+			res.json(req.file);
+		});
+	};
+	NodeUpload.prototype.upload_array_files = function (req, res, fieldName, types, config) {
+		req.extensions = (types && typeof types !== "undefined") ? (types instanceof Array) ? 
+			NodeUpload.removeDotsExtension(types) : NodeUpload.removeDotsExtension([types]) : NodeFilter._defTypes;
 
 		this.upload.limits = extend({
-            files: 2
-        }, NodeUpload._limits, config || {});
+			files: 2
+		}, this.defConfig, config || {});
 		this.upload.array(fieldName)(req, res, function(err) {
 			if(err || req.fileValidationError) {
 				return res.json({ "err": err || req.fileValidationError });
@@ -88,27 +92,43 @@ var NodeUpload = (function () {
 			}
 			res.json(req.files);
 		});
-    };
-    NodeUpload.prototype._filename = function (name) {
-        return name + Date.now();
-    };
-    NodeUpload.fileFilter = function (req, file, cb) {
-        var ext = path.extname(file.originalname).toLowerCase();
+	};
+	NodeUpload.prototype.upload_fields_files = function (req, res, fieldNames, types, config) {
+		req.extensions = (types && typeof types !== "undefined") ? (types instanceof Array) ? 
+			NodeUpload.removeDotsExtension(types) : NodeUpload.removeDotsExtension([types]) : NodeFilter._defTypes;
+
+		this.upload.limits = extend({
+			files: 2
+		}, this.defConfig, config || {});
+		this.upload.fields(fieldName)(req, res, function(err) {
+			if(err || req.fileValidationError) {
+				return res.json({ "err": err || req.fileValidationError });
+			} else if(!req.files) {
+				return res.json({ "err": "Files dont founded" });
+			}
+			res.json(req.files);
+		});
+	};
+	NodeUpload.prototype._filename = function (name) {
+		return name + Date.now();
+	};
+	NodeUpload.fileFilter = function (req, file, cb) {
+		var ext = path.extname(file.originalname).toLowerCase();
 		if (ext.substring(0, 1) == '.') { 
 			ext = ext.substring(1);
 		}
-        if (req.extensions.indexOf(ext) > -1) {
-            return cb(null, true);
-        }
-        req.fileValidationError = 'goes wrong on the extension';
-        return cb(null, false, new Error(req.fileValidationError));
-    };
-    NodeUpload._limits = {
-        fieldNameSize: 100
-    };
-    NodeUpload._defTypes = ["jpeg", "jpg", "png"];
-    NodeUpload._defDirectory = "./uploads/";
-    return NodeUpload;
+		if (req.extensions.indexOf(ext) > -1) {
+			return cb(null, true);
+		}
+		req.fileValidationError = 'goes wrong on the extension';
+		return cb(null, false, new Error(req.fileValidationError));
+	};
+	NodeUpload._limits = {
+		fieldNameSize: 100
+	};
+	NodeUpload._defTypes = ["jpeg", "jpg", "png"];
+	NodeUpload._defDirectory = "./uploads/";
+	return NodeUpload;
 }());
 
 exports = module.exports = NodeUpload;
